9ef221e90bbf4cf94f56337903659a4fd05cc241
jing8956 <jing8956@outlook.com>
2021-10-18 23:01:04 +0800

    Add TmdbEpisodeProvider

--- MoeACG.Jellyfin.Plugins/Providers/Tmdb/TmdbClientManager.fs
+++ MoeACG.Jellyfin.Plugins/Providers/Tmdb/TmdbClientManager.fs
@@ -35,6 +35,24 @@ type TmdbClientManager(memoryCache: IMemoryCache) =
         if path |> String.IsNullOrEmpty then null
         else tmDbClient.GetImageUrl(sizeThunk(), path).ToString()
 
+    member _.AsyncSearchSeries(name, language, year, cancellationToken) =
+        asyncGetOrRequest $"searchseries-{name}-{language}"
+        <| fun client ->
+                client.SearchTvShowAsync(
+                    query = name,
+                    language = language,
+                    firstAirDateYear = year,
+                    cancellationToken = cancellationToken) 
+        |> (fun computation -> async { let! searchResults = computation in return searchResults.Results })
+    member _.AsyncFindByExternalId(externalId, source, language, cancellationToken) = 
+        asyncGetOrRequest $"find-{source}-{externalId}-{language}"
+        <| fun client ->
+               client.FindAsync(
+                   source = source,
+                   id = externalId,
+                   language = TmdbUtils.normalizeLanguage language,
+                   cancellationToken = cancellationToken)
+
     member _.AsyncGetSeries(tmdbId, language, imageLanguages, cancellationToken) =
         asyncGetOrRequest $"series-{tmdbId}-{language}"
         <| fun client -> 
@@ -51,24 +69,6 @@ type TmdbClientManager(memoryCache: IMemoryCache) =
                        TvShowMethods.Videos |||
                        TvShowMethods.EpisodeGroups),
                    cancellationToken = cancellationToken)
-    member _.AsyncFindByExternalId(externalId, source, language, cancellationToken) = 
-        asyncGetOrRequest $"find-{source}-{externalId}-{language}"
-        <| fun client ->
-               client.FindAsync(
-                   source = source,
-                   id = externalId,
-                   language = TmdbUtils.normalizeLanguage language,
-                   cancellationToken = cancellationToken)
-    member _.AsyncSearchSeries(name, language, year, cancellationToken) =
-        asyncGetOrRequest $"searchseries-{name}-{language}"
-        <| fun client ->
-                client.SearchTvShowAsync(
-                    query = name,
-                    language = language,
-                    firstAirDateYear = year,
-                    cancellationToken = cancellationToken) 
-        |> (fun computation -> async { let! searchResults = computation in return searchResults.Results })
-
     member _.AsyncGetSeason(tvShowId, seasonNumber, language, imageLanguages, cancellationToken) =
         asyncGetOrRequest $"season-{tvShowId}-s{seasonNumber}-{language}"
         <| fun client ->
@@ -83,47 +83,70 @@ type TmdbClientManager(memoryCache: IMemoryCache) =
                        TvSeasonMethods.ExternalIds |||
                        TvSeasonMethods.Videos ),
                    cancellationToken = cancellationToken)
+    member _.AsyncGetEpisode(tvShowId, seasonNumber, episodeNumber, language, imageLanguages, cancellationToken) = 
+        asyncGetOrRequest $"episode-{tvShowId}-s{seasonNumber}-e{episodeNumber}-{language}"
+        <| fun client -> 
+               client.GetTvEpisodeAsync(
+                   tvShowId,
+                   seasonNumber,
+                   episodeNumber,
+                   language = TmdbUtils.normalizeLanguage language,
+                   includeImageLanguage = imageLanguages,
+                   extraMethods = (
+                       TvEpisodeMethods.Credits |||
+                       TvEpisodeMethods.Images |||
+                       TvEpisodeMethods.ExternalIds |||
+                       TvEpisodeMethods.Videos),
+                   cancellationToken = cancellationToken)
         
     member _.GetPosterUrl(posterPath) =
         getImageUrl tmDbClient.Config.Images.PosterSizes.Last posterPath 
     member _.GetBackdropUrl(backdropPath) =
         getImageUrl tmDbClient.Config.Images.BackdropSizes.Last backdropPath
     member _.GetDiscover() = tmDbClient.DiscoverTvShowsAsync()
+
+    member inline private this.GetPersonsOfCast (personType) (cast: Cast seq) =
+        let toPersonInfo (actor: Cast) =
+            let personInfo =
+                PersonInfo(
+                    Name = actor.Name.Trim(),
+                    Role = actor.Character,
+                    Type = personType,
+                    SortOrder = actor.Order,
+                    ImageUrl = this.GetPosterUrl(actor.ProfilePath))
+            if actor.Id > 0 then personInfo.SetProviderId(MetadataProvider.Tmdb, actor.Id.ToString("D"))
+            personInfo
+        cast
+        |> Seq.sortBy (fun actor -> actor.Order) 
+        |> Seq.take TmdbUtils.MaxCastMembers
+        |> Seq.map toPersonInfo
+    member inline private _.GetPersonsOfCrew(crews: Crew seq) =
+        let keepTypes = [| PersonType.Director; PersonType.Writer; PersonType.Producer |]
+        let isKeepType personType = keepTypes.Contains(personType, StringComparer.OrdinalIgnoreCase)
+        let toPersonInfo (r: struct {| Type: string; Crew: Crew |}) =
+            new PersonInfo(
+                Name = r.Crew.Name.Trim(),
+                Role = r.Crew.Job,
+                Type = r.Type)
+        crews
+        |> Seq.map (fun crew -> struct {| Type = TmdbUtils.mapCrewToPersonType crew; Crew = crew |})
+        |> Seq.filter (fun r -> r.Type |> isKeepType || r.Crew.Job |> isKeepType)
+        |> Seq.map toPersonInfo
     member inline this.GetPersons(hasCredits) = 
-        let ofCasts (cast: Cast seq) =
-            let toPersonInfo (actor: Cast) =
-                let personInfo =
-                    PersonInfo(
-                        Name = actor.Name.Trim(),
-                        Role = actor.Character,
-                        Type = PersonType.Actor,
-                        SortOrder = actor.Order,
-                        ImageUrl = this.GetPosterUrl(actor.ProfilePath))
-                if actor.Id > 0 then personInfo.SetProviderId(MetadataProvider.Tmdb, actor.Id.ToString("D"))
-                personInfo
-            cast
-            |> Seq.sortBy (fun actor -> actor.Order) 
-            |> Seq.take TmdbUtils.MaxCastMembers
-            |> Seq.map toPersonInfo
-        let ofCrews (crews: Crew seq) =
-            let keepTypes = [| PersonType.Director; PersonType.Writer; PersonType.Producer |]
-            let isKeepType personType = keepTypes.Contains(personType, StringComparer.OrdinalIgnoreCase)
-            let toPersonInfo (r: struct {| Type: string; Crew: Crew |}) =
-                new PersonInfo(
-                    Name = r.Crew.Name.Trim(),
-                    Role = r.Crew.Job,
-                    Type = r.Type)
-            crews
-            |> Seq.map (fun crew -> struct {| Type = TmdbUtils.mapCrewToPersonType crew; Crew = crew |})
-            |> Seq.filter (fun r -> r.Type |> isKeepType || r.Crew.Job |> isKeepType)
-            |> Seq.map toPersonInfo
         let toPersons (credits: Credits) =
             seq {
-                credits.Cast |> Obj.map ofCasts
-                credits.Crew |> Obj.map ofCrews
+                credits.Cast |> Obj.map (this.GetPersonsOfCast PersonType.Actor)
+                credits.Crew |> Obj.map this.GetPersonsOfCrew
             } |> Seq.map (Obj.defaultValue Seq.empty) |> Seq.concat
-        (^T: (member Credits: Credits) hasCredits)
-        |> Obj.map toPersons
+        (^T: (member Credits: Credits) hasCredits) |> Obj.map toPersons
+    member inline this.GetPersonsWithGuestStars(hasCredits) =
+        let toPersons (credits: CreditsWithGuestStars) =
+            seq {
+                credits.Cast |> Obj.map (this.GetPersonsOfCast PersonType.Actor)
+                credits.GuestStars |> Obj.map (this.GetPersonsOfCast PersonType.GuestStar)
+                credits.Crew |> Obj.map this.GetPersonsOfCrew
+            } |> Seq.map (Obj.defaultValue Seq.empty) |> Seq.concat
+        (^T: (member Credits: CreditsWithGuestStars) hasCredits) |> Obj.map toPersons
 
     interface IDisposable with 
         member _.Dispose() =
