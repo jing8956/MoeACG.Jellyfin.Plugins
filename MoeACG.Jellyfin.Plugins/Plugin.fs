9e5d329e42f70b3d676a80a51431862d45d0582d
jing8956 <jing8956@outlook.com>
2021-10-22 20:55:59 +0800

    整理

--- MoeACG.Jellyfin.Plugins/Plugin.fs
+++ MoeACG.Jellyfin.Plugins/Plugin.fs
@@ -15,15 +15,13 @@ type Plugin(paths, serializer) as this =
     static member Instance = instance
     static member Configuration = 
         (instance :> BasePlugin<_>).Configuration
-
+        
     override _.Id   = "38b7c2e3-9924-4b50-a808-541753db15e0" |> Guid.Parse
     override _.Name = "MoeACG.Jellyfin.Plugin"
 
     interface IHasWebPages with
         member this.GetPages() = 
-            seq { 
-               (new PluginPageInfo(
-                   Name = this.Name,
-                   EmbeddedResourcePath = $"{this.GetType().Namespace}.Configuration.configPage.html"
-               ))
-            }
+            new PluginPageInfo(
+                Name = this.Name,
+                EmbeddedResourcePath = $"{this.GetType().Namespace}.Configuration.configPage.html"
+            ) |> Seq.singleton
