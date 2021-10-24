42f9cdd96bae780ca5941b20f2e1187d3d7bc887
jing8956 <jing8956@outlook.com>
2021-10-24 16:09:15 +0800

    只关闭 tvShows 类型的 Resolver

--- MoeACG.Jellyfin.Plugins/ResolverIgnoreAllRule.fs
+++ MoeACG.Jellyfin.Plugins/ResolverIgnoreAllRule.fs
@@ -1,7 +1,16 @@
 // 路径复杂，不使用强制三层结构低效的 Resolver
 namespace MoeACG.Jellyfin.Plugins
 
+open System
+open MediaBrowser.Controller.Library
 open MediaBrowser.Controller.Resolvers
+open MediaBrowser.Model.Entities
 
-type ResolverIgnoreAllRule() = 
-    interface IResolverIgnoreRule with member _.ShouldIgnore(_, _) = true
+type ResolverIgnoreAllRule(manager: ILibraryManager) = 
+    interface IResolverIgnoreRule with 
+        member _.ShouldIgnore(_, parent) = 
+            let collectionType = manager.GetContentType(parent)
+            String.Equals(
+                collectionType, 
+                CollectionType.TvShows, 
+                StringComparison.OrdinalIgnoreCase)
