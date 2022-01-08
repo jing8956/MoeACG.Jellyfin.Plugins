a53d9d4f781974821399c6fe73b03ac71075a1aa
jing8956 <jing8956@outlook.com>
2022-01-08 16:22:40 +0800

    忽略器只忽略节目类型的媒体库

--- MoeACG.Jellyfin.Plugins/ResolverIgnoreRule.fs
+++ MoeACG.Jellyfin.Plugins/ResolverIgnoreRule.fs
@@ -2,31 +2,38 @@
 // 不适用于无法整理的媒体文件夹。
 namespace MoeACG.Jellyfin.Plugins
 
+open System
 open System.IO
 open MediaBrowser.Controller.Resolvers
 open MediaBrowser.Controller.Entities.TV
 open MoeACG.Jellyfin.Plugins.Resolvers
 open MediaBrowser.Controller.Entities
+open MediaBrowser.Controller.Library
+open MediaBrowser.Model.Entities
 
-type ResolverIgnoreRule() =
+type ResolverIgnoreRule(libraryManager:ILibraryManager) =
     interface IResolverIgnoreRule with 
         member _.ShouldIgnore(fileInfo, parent) =
-            match parent with
-            | :? Season | :? Series ->
-                if fileInfo.IsDirectory then true
-                else if fileInfo.Extension <> ".mp4" then true
-                else if fileInfo.Name |> MoeACGResolver.CanResolve |> not then true
-                else false
-            | :? AggregateFolder -> false
-            | :? UserRootFolder -> false
-            | :? Folder ->
-                let isMp4 (path:string) = Path.GetExtension(path) = ".mp4"
-                if fileInfo.IsDirectory |> not then true
-                else
-                    let files = Directory.EnumerateFiles(fileInfo.FullName)
-                    let canResolve (path:string) =
-                        let fileName = Path.GetFileNameWithoutExtension path
-                        isMp4 path && MoeACGResolver.CanResolve fileName
-                    let exists = files |> Seq.exists canResolve
-                    exists |> not
-            | _ -> true
+            let collectionType = libraryManager.GetConfiguredContentType(parent)
+            let isTvShows = String.Equals(collectionType, CollectionType.TvShows, StringComparison.OrdinalIgnoreCase)
+            if isTvShows then
+                match parent with
+                | :? Season | :? Series ->
+                    if fileInfo.IsDirectory then true
+                    else if fileInfo.Extension <> ".mp4" then true
+                    else if fileInfo.Name |> MoeACGResolver.CanResolve |> not then true
+                    else false
+                | :? AggregateFolder -> false
+                | :? UserRootFolder -> false
+                | :? Folder ->
+                    let isMp4 (path:string) = Path.GetExtension(path) = ".mp4"
+                    if fileInfo.IsDirectory |> not then true
+                    else
+                        let files = Directory.EnumerateFiles(fileInfo.FullName)
+                        let canResolve (path:string) =
+                            let fileName = Path.GetFileNameWithoutExtension path
+                            isMp4 path && MoeACGResolver.CanResolve fileName
+                        let exists = files |> Seq.exists canResolve
+                        exists |> not
+                | _ -> true
+            else false
